77a46f48067881952a52e6829341fd2026924fa5
rawk akani <lionelmbankeu@gmail.com>
2023-12-03 22:03:33 +0000

    cleaned up errors

--- src/app/main-features/attendance-inner/attendance-inner.component.ts
+++ src/app/main-features/attendance-inner/attendance-inner.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AttendanceService } from '../../services/attendance.service';
+import { AttendanceService } from '../../services/attendance/attendance.service';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
